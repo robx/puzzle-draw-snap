42d523995509606e38f3298d5e2dc82471b906d4
Robert Vollmert <rvollmert@gmx.net>
2014-04-01 22:47:20 +0200

    among others, update to new parsing model

--- src/Main.hs
+++ src/Main.hs
@@ -11,14 +11,14 @@ import Diagrams.BoundingBox
 import Diagrams.Backend.SVG
 
 import Data.Yaml
-import Data.Aeson (Result(..))
 
 import Diagrams.TwoD.Puzzles.PuzzleDraw
 import Diagrams.TwoD.Puzzles.Puzzle
-import Data.Puzzles.ReadPuzzle (TypedPuzzle)
+import Data.Puzzles.Read (TypedPuzzle(..))
 
-import Data.ByteString (ByteString)
-import Data.ByteString.Lazy (toStrict)
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import Text.Blaze.Svg.Renderer.Text (renderSvg)
 
 main :: IO ()
@@ -28,34 +28,18 @@ site :: Snap ()
 site = route [ ("puzzle", puzzlePostHandler) ] <|>
        dir "static" (serveDirectory "static")
 
-diagramHandler :: Int -> Snap ()
-diagramHandler n = serveDiagram (Dims (fromIntegral n * 100) 100) (diagram n)
-
-fail400 :: Snap ()
-fail400 = do
+fail400 :: String -> Snap ()
+fail400 e = do
    modifyResponse $ setResponseStatus 400 "Bad Request"
-   writeBS "400 error"
+   writeBS . C.pack $ "Bad request: " ++ e
    r <- getResponse
    finishWith r
-
-diagramPostHandler :: Snap ()
-diagramPostHandler = do
-    body <- readRequestBody 4096
-    case decode (toStrict body) of
-        Just n  -> diagramHandler n
-        Nothing -> fail400
-
-diagram :: Int -> Diagram B R2
-diagram n = hcat (replicate n (circle 1.0))
 
 serveDiagram :: SizeSpec2D -> Diagram B R2 -> Snap ()
 serveDiagram sz d = do
     let svg = renderDia SVG (SVGOptions sz Nothing) d
     modifyResponse $ setContentType "image/svg+xml"
     writeLazyText $ renderSvg svg
-
-cmtopoint :: Double -> Double
-cmtopoint = (* 28.3464567)
 
 cmtopix :: Double -> Double
 cmtopix = (* 40)
@@ -66,14 +50,17 @@ dwidth = fst . unr2 . boxExtents . boundingBox
 sizeServeDiagram :: Diagram B R2 -> Snap ()
 sizeServeDiagram d = serveDiagram (Width . cmtopix . dwidth $ d) d
 
-decodeAndDrawPuzzle :: ByteString -> Maybe (Diagram B R2, Diagram B R2)
-decodeAndDrawPuzzle = maybe Nothing drawP . decode
+decodeAndDrawPuzzle :: OutputChoice -> B.ByteString ->
+                       Either String (Diagram B R2)
+decodeAndDrawPuzzle oc b = decodeEither b >>= drawP
   where
-    drawP :: TypedPuzzle -> Maybe (Diagram B R2, Diagram B R2)
-    drawP tp = case drawPuzzle tp of Error   _ -> Nothing
-                                     Success d -> Just d
+    drawP :: TypedPuzzle -> Either String (Diagram B R2)
+    drawP (TP t p ms) = parseEither (handleP oc t) (p, ms)
+    handleP :: OutputChoice -> String -> (Value, Maybe Value) -> Parser (Diagram B R2)
+    handleP DrawPuzzle   = handle drawPuzzle' fail
+    handleP DrawSolution = handle drawSolution' fail
+    handleP DrawExample  = handle drawExample' fail
     
-
 getOutputChoice :: Snap OutputChoice
 getOutputChoice = do
     ocs <- maybe "puzzle" id <$> getParam "output"
@@ -85,6 +72,6 @@ puzzlePostHandler :: Snap ()
 puzzlePostHandler = do
     o <- getOutputChoice
     body <- readRequestBody 4096
-    case decodeAndDrawPuzzle (toStrict body) of
-        Just d  -> sizeServeDiagram (draw d o)
-        Nothing -> fail400
+    case decodeAndDrawPuzzle o (BL.toStrict body) of
+        Left e   -> fail400 e
+        Right d  -> sizeServeDiagram d
